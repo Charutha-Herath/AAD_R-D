a6bc9a1736a29c56ed17de29548be064f8a48264
Charutha-Herath <charuh2023@gmail.com>
2024-07-18 21:11:53 +0530

    updated Deserialization

--- src/main/java/org/example/Deserialization.java
+++ src/main/java/org/example/Deserialization.java
@@ -1,9 +1,23 @@
 package org.example;
 
 
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
 
 public class Deserialization {
     public static void main(String[] args) {
+        Animal animal = null;
 
+        try (FileInputStream fileIn = new FileInputStream("animal.json");
+             ObjectInputStream in = new ObjectInputStream(fileIn)) {
+            animal = (Animal) in.readObject();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (animal != null) {
+            System.out.println("Name: " + animal.name);
+            System.out.println("Species: " + animal.species);
+        }
     }
 }
