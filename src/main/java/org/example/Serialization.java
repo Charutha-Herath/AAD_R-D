44b630eacc1791290b6b8c8a4c5af41e3bceebb6
Charutha-Herath <charuh2023@gmail.com>
2024-07-18 21:06:45 +0530

    updated Serialization

--- src/main/java/org/example/Serialization.java
+++ src/main/java/org/example/Serialization.java
@@ -1,8 +1,18 @@
 package org.example;
 
 
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+
 public class Serialization {
     public static void main(String[] args) {
+        Animal animal = new Animal("Frog","Amphibian");
 
+        try (FileOutputStream fileOut = new FileOutputStream("animal.json");
+             ObjectOutputStream out = new ObjectOutputStream(fileOut)) {
+            out.writeObject(animal);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
