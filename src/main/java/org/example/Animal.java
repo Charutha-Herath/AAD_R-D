fc8abf217c8db6182c73239f9982c26b9aa6402d
Charutha-Herath <charuh2023@gmail.com>
2024-07-18 20:52:50 +0530

    created animal/serialization

---
+++ src/main/java/org/example/Animal.java
@@ -0,0 +1,10 @@
+package org.example;
+
+
+
+import java.io.Serializable;
+
+public class Animal implements Serializable {
+
+
+}
---
+++ src/main/java/org/example/Serialization.java
@@ -0,0 +1,8 @@
+package org.example;
+
+
+public class Serialization {
+    public static void main(String[] args) {
+
+    }
+}
